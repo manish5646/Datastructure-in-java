bee99f9ce0ec0879c4d7b917034d48122b0fc44e
Manish  Mishra <manishweb9199@gmail.com>
2026-05-26 22:11:32 +0530

    Create Arenagram_1.java

---
+++ String/Arenagram_1.java
@@ -0,0 +1,19 @@
+public class Aenagram_1 {
+
+    public static void main(String[] args) {
+
+        String s1 = "java is easy";
+        String s2 = "";
+
+        for (int i = 0; i < s1.length(); i++) {
+
+            char ch = s1.charAt(i);
+
+            if (!s2.contains(ch + "")) {
+                s2 += ch;
+            }
+        }
+
+        System.out.println(s2);
+    }
+}
